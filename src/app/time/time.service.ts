6270f47edd9a9a2249234c934536d7ac48bc5857
asebastianpacific <asebastian@pacificspecialty.com>
2023-08-30 21:44:23 -0700

    update time every minute

--- src/app/time/time.service.ts
+++ src/app/time/time.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {map, Observable} from "rxjs";
+import {interval, map, Observable, startWith, switchMap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,10 @@ export class TimeService {
   }
 
   getDate(): Observable<Date> {
-    return this.http.get<any>('http://worldtimeapi.org/api/timezone/America/New_York')
+    const getter$ = this.http.get<any>('http://worldtimeapi.org/api/timezone/America/New_York')
       .pipe(map(ret => new Date(ret.datetime)));
+
+    //update every minute
+    return interval(60000).pipe(startWith(0), switchMap(i => getter$));
   }
 }
